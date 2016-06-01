f97959abc5825a3e5b56fdc0dd8e2607f543e630
eun-n <eunicena.12@gmail.com>
2016-06-01 16:22:37 -0700

    schema desciprtions

--- models/recipe.js
+++ models/recipe.js
@@ -2,11 +2,17 @@ var mongoose = require('mongoose');
 
 var BreedSchema = new mongoose.Schema({
   name: String,
+  //size is XS, S, M, L, XL
   size: String,
+  //image is a url
   image: String, 
+  //lifespan is the number of years
   lifespan: Number,
+	//training is easy, medium, or hard
   training: String,
+  //shedding is on a scale of 1-5
   shedding: Number,
+  //energylevel is on a scale of 1-5
   energylevel: Number
 });
 
