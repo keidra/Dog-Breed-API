f1d539341f6336102504a32b26d35356f681a6a8
keidra <keidrabaldwin@gmail.com>
2016-06-01 16:10:05 -0700

    cleared files

--- public/app/services.js
+++
@@ -1,8 +0,0 @@
-
-var services = angular.module('BreedServices', ['ngResource']);
-services.factory('BreedAPI', ['$resource', function($resource) {
-	return $resource('/api/breed/:id', {}, {
-		get: {method: 'GET', cache: false, isArray: false}
-	});
-}]);
-
