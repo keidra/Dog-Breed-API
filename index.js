3c9d32b653d85d21aebb1546d7ebc4769f7c7091
eun-n <eunicena.12@gmail.com>
2016-06-01 13:28:56 -0700

    mongoose variables

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
+//mongoose stuff
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/breed');
+
 app.listen(process.env.PORT || 3000);
